8aa20775bdb7115a4b4d256c766500ecbbfe8f16
ktimoney <kevin.timoney@est.tech>
2023-08-10 16:38:02 +0100

    Added docker-compose ranpm
    Issue-ID: NONRTRIC-903
    Change-Id: I882b6a9e8d123104240eed8337eef9739451b3ab
    Signed-off-by: ktimoney <kevin.timoney@est.tech>

---
+++ docker-proj/README.md
@@ -0,0 +1,91 @@
+## RANPM project in docker
+
+A docker compose project showing pm file flow from simulated network elements to filtered pm data.
+
+## Run in docker-desktop - docker only
+
+### Clone the ranpm repo:
+
+`git clone https://gerrit.nordix.org/local/oransc/ranpm`
+
+### Build Https server
+
+`cd ranpm/https-server`
+
+`./build.sh no-push`
+
+
+### Start demo
+
+WARNING: The setup scripts below will prune all unused docker volumes!
+
+INFO: Temporary files for some containers will be stored in subdirs under `/tmp`
+
+`cd ranpm/docker-proj`
+
+There are several ways to start and run the demo, with file or minio file storage, single or multi instances of data-file collector and kafka as well as a choice of sftp, ftpes or https.
+Additional configuration can be made in the setup script.
+
+It could be a good idea to clean any other running containers in docker to avoid port and container name clashes.
+
+Command usage: `docker-setup.sh 
+
+
+Example cmd: \
+`./docker-setup.sh 
+
+Let the script finish.
+
+If the script fails, make sure to clean the setup before attempting a new setup.
+
+`./docker-tear-down.sh`
+
+In addition, a `docker system prune` might be needed now and then.
+
+### Tools for monitoring
+
+Open browser to redpanda (kafka gui) - watch topics, messages etc\
+browser: `localhost:8780`
+
+Open brower to minio - available only if minio is given on the cmd line when starting the demo\
+user: admin pwd: adminadmin\
+browser: `localhost:9001`
+
+
+### Push data - basic
+
+File ready events can be pushed to the ves collector or pushed directly to the topic for file ready events (bypassing the ves collector).
+
+Push to ves collector:
+
+Usage: `push-to-ves-collector.sh <node-count> <num-of-events> <node-name-base> <file-extension> <num-servers>`
+
+Parameters/
+node-count - number of unique NEs\
+num-of-events - number of events per NE\
+node-name-base - NE name prefix\
+file-extension - xml or xml.gz
+num-servers - number of sftp/ftpes/https servers to simulate  NEs (10 is default)
+
+Usage: `push-to-file-ready-topic.sh <node-count> <num-of-events> <node-name-base> <file-extension> <num-servers>`
+
+Parameter: \
+Same as `push-to-ves-collector.sh`
+
+Once the events has been pushed the progress can be viewed in the monitoring tools described above.
+
+If several sets of data shall be pushed, just change the parameter `<node-name-base>` to make the new files unique.
+
+
+### Clean up
+
+Run the script to remove all docker containers.
+
+`./docker-tear-down.sh`
+
+To also cleanup files.
+
+`cd pm-file-flow-demo/scripts`
+
+`./clean-shared-volume.sh`
+
---
+++ docker-proj/scripts/push-to-ves-collector.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+#  ============LICENSE_START===============================================
+#  Copyright (C) 2023 Nordix Foundation. All rights reserved.
+#  ========================================================================
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ============LICENSE_END=================================================
+#
+
+SD=$(dirname -- "$0")
+echo "script-home: "$SD
+cd $SD
+CWD=$PWD
+
+NODE_COUNT=$1
+EVT_COUNT=$2
+NODE_NAME_BASE=$3
+FILE_EXT=$4
+SRV_COUNT=$5
+
+HTTPS_PORT=443
+
+print_usage() {
+    echo "Usage: push-to-ves-collector.sh <node-count> <num-of-events> <node-name-base> <file-extension> <num-servers>"
+    exit 1
+}
+echo $@
+if [ $# -lt 5 ]; then 
+    print_usage
+fi
+
+rm .out.json
+touch .out.json
+
+BEGINTIME=1665146700
+CURTIME=$BEGINTIME
+
+for (( i=0; i<$EVT_COUNT; i++)); do
+
+    echo "EVENT BATCH: $i of $EVT_COUNT events for $NODE_COUNT NODES - 1 FILE PER EVENT"
+    echo '{"eventList": [' > .out.json
+    first=0
+
+    arch="$(uname -s)"
+    if [[ $arch == "Darwin"* ]]; then
+        ST=$(date -r $CURTIME +'%Y%m%d.%H%M')
+        let CURTIME=CURTIME+900
+        ET=$(date -r $CURTIME +'%H%M')
+    else
+        ST=$(date -d @$CURTIME +'%Y%m%d.%H%M')
+        let CURTIME=CURTIME+900
+        ET=$(date -d @$CURTIME +'%H%M')
+    fi
+
+    for (( j=0; j<$NODE_COUNT; j++)); do
+
+            NO="$NODE_NAME_BASE-$j"
+            #FN="A20000626.2315+0200-2330+0200_$NO-$i.$FILE_EXT"
+            FN="A$ST+0200-$ET+0200_$NO-$i.$FILE_EXT"
+            let SRV_ID=$j%$SRV_COUNT
+            let SRV_ID=SRV_ID+1
+            #echo "NODE "$NO
+            echo "FILENAME "$FN
+            SRV="pm-https-server-$SRV_ID"
+            echo "HTTP SERVER "$SRV
+            URL="https://$SRV:$HTTPS_PORT/files/$FN"
+            EVT='{"commonEventHeader":{"startEpochMicrosec":15198378,"eventId":"FileReady_1797490e-10ae-4d48-9ea7-3d7d790b25e1","timeZoneOffset":"UTC+05.30","internalHeaderFields":{"collectorTimeStamp":"Wed,0907202211:56:02GMT"},"priority":"Normal","version":"4.0.1","reportingEntityName":"'$NO'","sequence":0,"domain":"notification","lastEpochMicrosec":151983,"eventName":"Notification_gnb-Ericsson_FileReady","vesEventListenerVersion":"7.0.1","sourceName":"'$NO'"},"notificationFields":{"notificationFieldsVersion":"2.0","changeType":"FileReady","changeIdentifier":"PM_MEAS_FILES","arrayOfNamedHashMap":[{"name":"'$FN'","hashMap":{"location":"'$URL'","fileFormatType":"org.3GPP.32.435#measCollec","fileFormatVersion":"V10","compression":"gzip"}}]}}'
+            if [ $first -ne 0 ]; then
+                echo "," >> .out.json
+            fi
+            first=1
+            echo "$EVT" >> .out.json
+    done
+    echo ']}' >> .out.json
+    RES=$(curl -s -X POST 'localhost:8080/eventListener/v7/eventBatch' --header 'Content-Type: application/json' --data-binary @.out.json)
+    echo $RES
+done
+
