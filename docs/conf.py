538f332a21d156617e0e8cd106e4ec6389b88c47
ktimoney <kevin.timoney@est.tech>
2023-09-04 10:59:17 +0100

    Update readthedocs config
    Issue-ID: NONRTRIC-921
    Change-Id: Idde1242268b3efe63974b03b0b8df544772c47c8
    Signed-off-by: ktimoney <kevin.timoney@est.tech>
    (cherry picked from commit 7904b3a47ef4ea81f026543a13cec0f0ef4456fd)

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
 #
-#       http://www.apache.org/licenses/LICENSE-2.0
+#        http://www.apache.org/licenses/LICENSE-2.0
 #
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,7 +16,7 @@
 #
 
 # NB: This is the only conf.yaml file used by the upstream readthedocs job  (ref '.readthedocs.yaml')
-#     Where possible include contents of the sub-folders' 'conf.yaml' files here if appropriate
+#    Where possible include contents of the sub-folders' 'conf.yaml' files here if appropriate
 
 from docs_conf.conf import *
 
