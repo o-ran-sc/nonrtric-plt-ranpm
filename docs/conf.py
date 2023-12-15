a2885c624d08ba33d1b8498dba042e02c6285708
JohnKeeney <john.keeney@est.tech>
2023-12-15 22:39:15 +0000

    Make it easier to build doc & subsites
    Change-Id: Iddaabde747de489b96e9a4c7937c5bf18accd2cb
    Signed-off-by: JohnKeeney <john.keeney@est.tech>
    Issue-ID: NONRTRIC-958

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,6 @@
 #  ============LICENSE_START===============================================
 #  Copyright (C) 2021-2023 Nordix Foundation. All rights reserved.
+#  Copyright (C) 2023 OpenInfra Foundation Europe. All rights reserved.
 #  ========================================================================
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -20,9 +21,12 @@
 
 from docs_conf.conf import *
 
-branch = 'latest'
+branch = 'i-release'
+subsitesbranch = 'latest'
+
 baseurl = 'https://docs.o-ran-sc.org/projects/'
 selfurl = '%s/o-ran-sc-nonrtric-plt-ranpm/en/%s' %(baseurl, branch)
+subsitesurl = '%s/o-ran-sc-nonrtric-plt-ranpm/en/%s' %(baseurl, subsitesbranch)
 
 linkcheck_ignore = [
     'http://localhost.*',
@@ -39,9 +43,9 @@ extensions = [
 intersphinx_mapping = {}
 intersphinx_mapping['nonrtric'] = ('%s/o-ran-sc-nonrtric/en/%s' %(baseurl, branch), None)
 ## Note there is a circular dependency here - sub-project pages must exist before they can be checked
-intersphinx_mapping['influxlogger'] = ('%s/influxlogger' % selfurl, None)
-intersphinx_mapping['datafilecollector'] = ('%s/datafilecollector' % selfurl, None)
-intersphinx_mapping['pmproducer'] = ('%s/pmproducer' % selfurl, None)
-intersphinx_mapping['pm-file-converter'] = ('%s/pm-file-converter' % selfurl, None)
+intersphinx_mapping['influxlogger'] = ('%s/influxlogger' %subsitesurl, None)
+intersphinx_mapping['datafilecollector'] = ('%s/datafilecollector' %subsitesurl, None)
+intersphinx_mapping['pmproducer'] = ('%s/pmproducer' %subsitesurl, None)
+intersphinx_mapping['pm-file-converter'] = ('%s/pm-file-converter' %subsitesurl, None)
 
 intersphinx_disabled_reftypes = ["*"]
