56e6f9da7fb5904d56146c87af0668c69c20b3eb
aravind.est <aravindhan.a@est.tech>
2025-08-13 10:21:04 +0100

    Add Deprecation Notice for RANPM Installation scripts
    it/dep SMO-Install offers separate flavour for RANPM installation. Hence this is deprecated.
    
    Issue-ID: NONRTRIC-1082
    Change-Id: If7f9f00c7dc3d649fea2c99fc598dbd2451b7a4a
    Signed-off-by: aravind.est <aravindhan.a@est.tech>

--- install/install-ranpm.sh
+++ install/install-ranpm.sh
@@ -2,6 +2,7 @@
 
 #  ============LICENSE_START===============================================
 #  Copyright (C) 2023 Nordix Foundation. All rights reserved.
+#  Copyright (C) 2023-2025 OpenInfra Foundation Europe. All rights reserved.
 #  ========================================================================
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -18,6 +19,13 @@
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "${ROOT_DIR}"
+
+# Deprecated: This script is deprecated and will be removed in future releases.
+echo "******************** DEPRECATION NOTICE ************************"
+echo "This installation script is no longer maintained or supported."
+echo "Please follow the latest installation https://github.com/o-ran-sc/it-dep/tree/master/smo-install#readme for the recommended and actively maintained setup."
+echo -e "A dedicated flavour configuration for installing RANPM is available here (https://github.com/o-ran-sc/it-dep/tree/master/smo-install/helm-override/ranpm.)"
+echo -e "*****************************************************************\n"
 
 # Array of installation scripts
 scripts=("install-nrt.sh" "install-pm-log.sh" "install-pm-influx-job.sh" "install-pm-rapp.sh")
